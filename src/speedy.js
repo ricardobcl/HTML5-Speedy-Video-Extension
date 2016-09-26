61335412d40da3129545f4cf8999ddce8f59b98d
Ricardo Gonçalves <tome.wave@gmail.com>
2016-09-26 15:32:42 +0100

    Now detects dynamically changed URLs and tries to find the video tag.

--- src/speedy.js
+++ src/speedy.js
@@ -1,15 +1,17 @@
 
 // defaults
-var default_speed = 1.0; // initial playback speed 
+var default_speed = 1.0; // initial playback speed
 var default_delta = 0.2; // smallest increment or decrement of playback speed
 var default_skip_small = 2; // number of seconds to (small) skip or rewind the video
 var default_skip_big = 10; // number of seconds to (big) skip or rewind the video
 var default_autoplay = false; // flag for autoplay the video
-var debug = false; // flag to disable or enable console log debug info
+var debug = true; // flag to disable or enable console log debug info
 var default_stop_video_timeout = 15000; // timeout in milliseconds before stop pausing the video
-var max_tries_finding_video = 25; // max number of tries to finding the video
-var youtube_playerbar_name_class = ".html5-player-chrome"; // name class of youtube player bar 
-var videojs_playerbar_name_class = ".vjs-control-bar"; // name class of VideoJS player bar 
+var max_tries_finding_video = 150; // max number of tries to finding the video
+var youtube_playerbar_name_class = ".html5-player-chrome"; // name class of youtube player bar
+var videojs_playerbar_name_class = ".vjs-control-bar"; // name class of VideoJS player bar
+// var netflix_playerbar_name_class = ".player-control-bar"; // name class of Netflix player bar
+var netflix_playerbar_name_class = ".player-status"; // name class of Netflix player bar
 
 // internal variables
 var tries = 0;
@@ -17,13 +19,40 @@ var my_video; // = undefined;
 var ignore_keyshorts = false;
 var timeout_finding_video;
 var timeout_pausing_video;
+var timeout_finding_status_bar;
+var oldLocation = window.location.href;
 
 // necessary test to only launch the extension 1 time per page (at least on safari)
 if (window.top === window) {
     log("Starting Speedy Video");
-    // try running setup() 4 times per second until we find the video tag or reach
-    // the maximum number of tries 'max_tries_finding_video'
-    timeout_finding_video = window.setInterval(setup, 250);
+    if (oldLocation.indexOf("watch") >= 0 || oldLocation.indexOf("Anime") >= 0 ) {
+        // try running setup() 4 times per second until we find the video tag or reach
+        // the maximum number of tries 'max_tries_finding_video'
+        timeout_finding_video = window.setInterval(setup, 250);
+    } else {
+        log("URL changed, but no \"watch\" in it.");
+    }
+    window.setInterval(check_changed_url, 100);
+}
+
+function check_changed_url() {
+    if(window.location.href != oldLocation) {
+        log("old: " + oldLocation);
+        log("new :" + window.location.href);
+        // update new url
+        oldLocation = window.location.href
+        if (oldLocation.indexOf("watch") >= 0 || oldLocation.indexOf("Anime") >= 0 ) {
+            // try finding a new video tag
+            clearInterval(timeout_pausing_video);
+            clearInterval(timeout_finding_video);
+            tries = 0;
+            // try running setup() 4 times per second until we find the video tag or reach
+            // the maximum number of tries 'max_tries_finding_video'
+            timeout_finding_video = window.setInterval(setup, 250);
+        } else {
+            log("URL changed, but no \"watch\" in it.");
+        }
+    }
 }
 
 function setup() {
@@ -39,7 +68,7 @@ function setup() {
         // try to stop (or not) the autoplay
         deal_with_autoplay();
         // add buttons to the video player to show current speed and to speed up or down
-        add_buttons_player();
+        timeout_finding_status_bar = window.setInterval(add_buttons_player, 250);
         // set up keyboard shortcuts
         setup_shorcuts();
         // add events to toggle keyboard shortcuts on/off when entering an input box
@@ -65,23 +94,36 @@ function deal_with_autoplay() {
         // this expires after 'default_stop_video_timeout' milliseconds or
         // when the user uses the space key to resume the video.
         timeout_pausing_video = setInterval( function(){
-            my_video.pause();
+            if(my_video !== undefined) {
+                my_video.pause();
+            }
         }, 100);
         setTimeout(function(){
             clearInterval(timeout_pausing_video);
+            clearInterval(timeout_finding_status_bar);
         }, default_stop_video_timeout);
     }
 }
 
+// TODO: show the current speed as an overlay on top of the video, when changing speed
+function add_speed_message_overlay() {
+}
+
 function add_buttons_player() {
-    $("<div id=\"speedy_extension_addon_2_player\"></div>").appendTo(youtube_playerbar_name_class);
-    $("<div id=\"speedy_extension_addon_2_player\"></div>").appendTo(videojs_playerbar_name_class);
-    $("<button id=\"speedy_speed_down\" class=\"button\">--</button>").appendTo("#speedy_extension_addon_2_player");
-    $("<b class=\"tag\"><span id=\"speedy_video_speed\">1.00</span>x</b>").appendTo("#speedy_extension_addon_2_player");
-    $("<button id=\"speedy_speed_up\" class=\"button\">++</button>").appendTo("#speedy_extension_addon_2_player");
-    $('#speedy_video_speed').html(my_video.playbackRate.toFixed(2));
-    $("#speedy_speed_down").click(function() { delta_speed( - default_delta); });
-    $("#speedy_speed_up" ).click(function() { delta_speed( + default_delta); });
+    log("Trying to add the control buttons on the player status bar");
+    if($("#speedy_extension_addon_2_player")[0] == undefined) {
+        $("<div id=\"speedy_extension_addon_2_player\"></div>").appendTo(youtube_playerbar_name_class);
+        $("<div id=\"speedy_extension_addon_2_player\"></div>").appendTo(videojs_playerbar_name_class);
+        $("<span id=\"speedy_extension_addon_2_player\"></span>").appendTo(netflix_playerbar_name_class);
+        $("<button id=\"speedy_speed_down\" class=\"button\">--</button>").appendTo("#speedy_extension_addon_2_player");
+        $("<b class=\"tag\"><span id=\"speedy_video_speed\">1.00</span>x</b>").appendTo("#speedy_extension_addon_2_player");
+        $("<button id=\"speedy_speed_up\" class=\"button\">++</button>").appendTo("#speedy_extension_addon_2_player");
+        $('#speedy_video_speed').html(my_video.playbackRate.toFixed(2));
+        $("#speedy_speed_down").click(function() { delta_speed( - default_delta); });
+        $("#speedy_speed_up" ).click(function() { delta_speed( + default_delta); });
+    } else {
+        clearInterval(timeout_finding_status_bar);
+    }
 }
 
 function delta_speed(x){
@@ -117,6 +159,10 @@ function setup_shorcuts() {
                     absolute_speed(2);
                     break;
                 case 51 : // 3
+                    log_prevent("speed 2.5",event);
+                    absolute_speed(2.5);
+                    break;
+                case 52 : // 4
                     log_prevent("speed 3",event);
                     absolute_speed(3);
                     break;
